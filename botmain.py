4c7af98b73db930f49349d048ca105e810b10c91
Sertzu <sertzusc@gmail.com>
2023-12-06 19:28:36 +0100

    test

--- botmain.py
+++ botmain.py
@@ -22,7 +22,7 @@ def getaverages():
 
 def main():
     time.sleep(60)
-    print("Trying to get Token!")
+    print("Trying to get Token!!")
     token = os.getenv('BOT_TOKEN')
     if token is None:
         with open("token", 'r') as file:
